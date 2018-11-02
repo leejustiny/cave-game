63ee8b754da8a3ed53d0b3ade2d80d4980beb0c0
Luke <scu.imaginarium@gmail.com>
2018-11-02 15:54:57 -0700

    VR Set Up

---
+++ Assets/Scripts/ReplaceOre.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReplaceOre : MonoBehaviour {
+
+    public GameObject ore;
+    public GameObject diamond;
+    
+    
+  
+    // Use this for initialization
+    void Start () {
+        ore = GameObject.Find("UnbrokenOre");
+        diamond = GameObject.Find("Diamond");
+        ore.SetActive(true);
+        diamond.SetActive(false);
+        
+}
+
+    public void ReplaceWithDiamond()
+    {
+        ore.SetActive(false);
+        diamond.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Pickaxe"))
+        {
+            ReplaceWithDiamond();
+        }
+
+    }
+}
---
+++ Assets/Scripts/TurnFlashOn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnFlashOn : MonoBehaviour {
+
+    public GameObject flashlight;
+    public GameObject spotlight;
+    public Light beamlight;
+    
+
+    // Use this for initialization
+    void Start () {
+		flashlight = GameObject.Find("flashlight");
+        spotlight = GameObject.Find("spotlight");
+        beamlight = spotlight.GetComponent<Light>();
+
+        spotlight.setActive(false);
+    }
+
+    public void LightOn()
+    {
+        spotlight.setActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void onTriggerEnter(Collider hand)
+    {
+        if (coll.CompareTag("hand"))
+        {
+         
+            LightOn();
+        }
+
+    }
+}
